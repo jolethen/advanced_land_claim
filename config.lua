5a3241e13201bfddf340633b856dc9f70f4a1b43
jolethen <155651557+jolethen@users.noreply.github.com>
2025-10-10 22:28:29 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+CLAIM_LIMIT = 5
+HEIGHT_MIN = -10
+HEIGHT_MAX = 40
+
+PRICE_ITEMS = {
+    {item = "default:gold_ingot", rate = 0.05},
+    {item = "default:diamond", rate = 0.01},
+    {item = "default:emerald", rate = 0.02},
+}
+
+SHOWCLAIM_DURATION = 15 -- seconds
+SHOWCLAIM_NODE = "default:torch" -- temporary border marker
