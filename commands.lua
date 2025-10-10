75b41814f25a2e25134ac0acab1344f2bb4fb64a
jolethen <155651557+jolethen@users.noreply.github.com>
2025-10-10 22:30:28 +0530

    Create commands.lua

---
+++ commands.lua
@@ -0,0 +1,4 @@
+local function calc_cost(size, rate) return math.ceil(size*size*rate) end
+
+-- Claim, unclaim, add/remove players, myclaims (same as previous version)
+-- Copy the full commands.lua logic from previous modular version
